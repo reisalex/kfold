bc5175a0b4230c2acc28a6af74c21b9a01002e50
alex.reis <areis1992@gmail.com>
2018-12-08 18:51:00 -0500

    Adding kfold f2py code and KFOLDWrapper.py. Debugging code. Bad pointer somewhere.

---
+++ src/python/KFOLDWrapper.py
@@ -0,0 +1,125 @@
+
+import time
+import multiprocessing
+from contextlib import contextmanager
+from itertools import repeat
+
+import kfold
+import tqdm
+
+# load PyVRNA interface to ViennaRNA
+from PyVRNA import PyVRNA
+ViennaRNA = PyVRNA(parameter_file='rna_turner1999.par',pyindex=True)
+
+# default options
+NCPUS = multiprocessing.cpu_count()
+
+# get log-spaced time steps from 0.01 to tmax
+def get_trange(tmax):
+    dt = 0.01
+    io = 1
+    trange = [dt]
+    while (dt < tmax):
+        trange.append(trange[-1]+dt)
+        io += 1
+        if (io > 9):
+            io = 1
+            dt *= 10.0
+    return trange
+
+# return a dictionary containing the default options for simulation
+def get_default_options(seq):
+    maxtime=1000.0
+    fold=ViennaRNA.RNAfold(seq)
+    return dict(
+        fold0='.'*len(seq),               # starting structure
+        foldf=fold.structure,             # first passage time search structure
+        ef=0.95*fold.energy,              # energy first passage time threshold value
+        nsim=1,                           # number of simulations internal to kfold (serial)
+        tmax=maxtime,                     # total simulation time (kfold units)
+        trange=get_trange(maxtime),       # time points at which to collect data
+        pynsim=1000                       # number of simulations through multiprocessing (parallel)
+        )                                 # total number of simulations ends up being nsim*pynsim
+
+# check options dictionary passed by a user (improve later)
+def check_options(seq,optsdict):
+    assert len(optsdict['fld0'])==len(seq)
+    assert len(optsdict['fldf'])==len(seq)
+    assert isinstance(optsdict['ef'],float)
+    assert isinstance(optsdict['nsim'],int)
+    assert isinstance(optsdict['tmax'],float)
+    assert isinstance(optsdict['trange'],list)
+    assert isinstance(optsdict['pynsim'],int)
+    return optsdict
+
+# generate tuple for pool.map from dictionary
+def make_kfold_input_tuple(seq,optsdict):
+    return tuple([seq]+[optsdict[x] for x in ['fold0','foldf','ef','nsim','tmax','trange']])
+
+# helper function to call kfold with pool.map
+def kfold_unpack(args):
+    return kfold_wrap(*args)
+
+# wrap the kfold lib
+# can be called to simulate RNA without multiprocessing
+# Inputs
+#   seq    :: RNA sequence (string)
+#   fold0  :: starting structure to initialize trajectory from (string)
+#   foldf  :: structure to monitor for when computing the first passage time distribution (string)
+#   ef     :: free energy to monitor for when computing the energy first passage time distribution (float, kcal/mol)
+#   nsim   :: number of simulations to run serially WITHIN kfold subroutine (int)
+#   tmax   :: total kfold simulation time (float, required to be a multiple of 10.0)
+#   trange :: 
+# Returns
+#   folds  :: list of lists with dot-parantheses folds
+#   dG     :: list of lists with free energies (kcal/mol)
+#   fpt    :: list of first passage times (kfold a.u.)
+#   efpt   :: list of energy first passage times (kfold a.u.)
+def kfold_wrap(seq,fold0,foldf,ef,nsim,tmax,trange):
+    traj,dG,fpt,efpt = kfold.run(seq,fold0,foldf,ef,nsim,tmax)
+    folds = [["".join(traj[i][j][:len(seq)]) for j in xrange(len(trange))] for i in xrange(nsim)]
+    return folds,dG,fpt,efpt
+
+# Python 3.3 this can be all replaced with pool.starmap() which accepts a sequence of
+# argument tuples, and then automatically unpacks the arguments from each tuple; see:
+# https://docs.python.org/dev/library/multiprocessing.html#multiprocessing.pool.Pool.starmap
+@contextmanager
+def poolcontext(*args,**kwargs):
+    print "Initializing multiprocessing pool...",
+    pool = multiprocessing.Pool(*args, **kwargs)
+    print "Ready."
+    yield pool
+    pool.terminate()
+
+# main function to simulate list of sequences
+# Arguments
+#   sequences :: list of RNA sequences to simulate
+#   options   :: dictionary of options for all simulations, see function `get_default_options`
+#   optsfxn   :: optional function to return options, by default uses `get_default_options` if <options> argument is None
+#   N         :: number of processes to instance for multiprocessing (uses default CPU count otherwise)
+def run(sequences,options=None,optsfxn=get_default_options,N=NCPUS):
+    
+    # check inputs
+    assert isinstance(sequences,list), 'Input argument <sequences> for function `simulate_rna` should be a list of sequence(s).'
+
+    # simulate using python's multiprocessing
+    with poolcontext(processes=N) as pool:
+        for seq in sequences:
+            if options is None: opts=optsfxn(seq)
+            else: opts=check_options(seq,options)
+            print """\nSimulating: {} (first 50 nucleotides shown) up to {} kfold time,
+            for {} total trajectories, with {} cores...""".format(seq[:50],opts['tmax'],opts['pynsim']*opts['nsim'],N)
+            inputs = make_kfold_input_tuple(seq,opts)
+            # results = pool.map(kfold_unpack,repeat(input_tuple,options['pynsim']))
+            for x in tqdm.tqdm(pool.imap_unordered(kfold_unpack,repeat(inputs,opts['pynsim'])), total=opts['pynsim']):
+                pass
+
+def test():
+    sequences = [
+    'ATTCTTAGGGGCGGAGCGGCGCGGCGCCCCTAAGAATTTTT',
+    'AGAGGCGCCAAACAGGGGCCCGGAAACGGGCGCTGTTTTTT'
+    ]
+    run(sequences)
+
+if __name__ == "__main__":
+    test()
---
+++ src/python/kfold.f90
@@ -0,0 +1,200 @@
+! =============================================================================
+! Program: KFOLD
+! 
+! Description: A program for computing the folding kinetics of an RNA
+!              sequence using Turner energies.
+!
+! History:
+!
+! Version    Date         Comment
+! --------   ----------   -----------------------
+!            01/01/2015   Original Code
+!
+! Dependancies:
+!
+! Modules - Class_RNAFold, RNAVar
+! Functions -
+! Subroutines - READDATA SSAREACTION V2CT
+!
+! Author(s): Eric Dykeman, Alex Reis
+!            Copyright (c) 2015 (Please Refer to LICENCE)
+!
+! =============================================================================
+
+      SUBROUTINE KFOLD(FASTA,STR0,STRF,EF,NSIM,TMAX,TRAJOUT,EOUT,FPT,EFPT)
+
+        USE RNAVar, ONLY : mxnt
+
+        USE Class_RNAFold
+
+        IMPLICIT NONE
+
+        !=== VARIABLES FOR F2PY INTERFACE ===!
+
+        CHARACTER (LEN=mxnt+1), INTENT(IN) :: fasta
+        CHARACTER (LEN=mxnt),   INTENT(IN) :: str0
+        CHARACTER (LEN=mxnt),   INTENT(IN) :: strf
+        REAL,                   INTENT(IN) :: ef
+        INTEGER,                INTENT(IN) :: nsim
+        DOUBLE PRECISION,       INTENT(IN) :: tmax
+
+        REAL,                   INTENT(OUT) :: eout(nsim,100)
+        DOUBLE PRECISION,       INTENT(OUT) :: fpt(nsim)
+        DOUBLE PRECISION,       INTENT(OUT) :: efpt(nsim)
+        CHARACTER,              INTENT(OUT) :: trajout(nsim,100,mxnt+1)
+
+        !=== VARIABLES ===!
+
+        TYPE(RNA_STRUC) :: rna
+
+        CHARACTER :: seq(mxnt)
+        CHARACTER :: fld(mxnt)
+
+        INTEGER :: iseq(mxnt)
+        INTEGER :: ibpi(mxnt)
+        INTEGER :: ibpf(mxnt)
+
+        INTEGER :: i,j,k,n,nn,is,io,oindex
+        INTEGER :: isim,narg,iseed,seedgen,pid
+
+        DOUBLE PRECISION :: random,tstart,time
+        DOUBLE PRECISION :: tout,dt
+
+        LOGICAL :: fstop,estop
+
+        REAL :: e
+
+        !=== DEFAULT SETTINGS ===!
+
+        !=== Max Time (microseconds) ===!
+
+        tstart = 0.0d0
+
+        ! iseed = 61928712
+
+        fpt(:) = 1.0d12
+
+        pid = GETPID()
+        iseed = SEEDGEN(pid)
+
+        !=== Inital / Final Structure ===!
+
+        ibpi(:) = 0
+        ibpf(:) = 0
+
+        nn = LEN_TRIM(fasta)
+
+        IF ( nn > mxnt ) THEN
+          WRITE(*,*)'ERROR: Maximum number of nt = ',mxnt
+          WRITE(*,*)'Increase mxnt in rnavar.f90'
+          STOP
+        ENDIF
+
+        !=== Convert fasta, str0, strf ===!
+
+        READ(fasta,'(10000A1)')(seq(k),k=1,nn)
+
+        READ(str0,'(10000A1)')(fld(k),k=1,nn)
+        CALL V2CT (ibpi,fld,'C',nn)
+
+        READ(strf,'(10000A1)')(fld(k),k=1,nn)
+        CALL V2CT (ibpf,fld,'C',nn)
+
+        IF ( ALL( (/1.0d0, 10.0d0, 100.0d0, 1000.0d0, 10000.0d0, 100000.0d0/)/=tmax ) )THEN
+          WRITE(*,*)'ERROR: tmax must be an nth power of 10, i.e. 1, 10, 100'
+          CALL EXIT(0)
+        ENDIF
+
+        !=== SECTION 1 - Read in data ===!
+
+        CALL READDATA
+
+        !=== Setup RNA ===!
+
+        CALL CONVERT (seq,iseq,nn)
+        CALL SETUPNUC (nn)
+
+        rna% seq(:) = seq(1:nn)
+        rna% iseq(:) = iseq(1:nn)
+        rna% n = nn
+
+
+        !=== SECTION 2 - Perform RNA Kinetics ===! 
+
+        DO isim=1,nsim
+
+          io = 1
+          dt = 1.0d-2
+
+          tout = dt
+          time = tstart
+
+          fstop = .TRUE.
+          estop = .TRUE.
+
+          ! Initial structure always provided
+          rna% ibsp(:) = ibpi(:)
+ 
+          CALL LOOP_INIT (rna)
+
+          !=== STOCHASTIC SIMULATION ===!
+
+          DO WHILE ( time < tmax )
+
+            CALL SSAREACTION (rna,iseed,time,tout,fld,e)
+
+            !=== Increment tout ===!
+
+            IF ( time > tout ) THEN
+
+              trajout(isim,oindex,1:nn) = fld(1:nn)
+              eout(isim,oindex) = e
+
+              oindex = oindex + 1
+
+              tout = tout + dt
+
+              io = io + 1
+
+              IF ( io > 9 ) THEN
+                io = 1
+                dt = dt * 10.0d0
+              ENDIF
+
+            ENDIF
+
+            !=== Check for stop structure ===!
+
+            IF ( fstop ) THEN
+
+              j = 0
+
+              DO i=1,nn
+              IF ( ibpf(i) == rna%ibsp(i) ) j = j + 1
+              ENDDO
+
+              IF ( j == nn ) THEN
+                fpt(isim) = time
+                fstop = .FALSE.
+              ENDIF
+
+            ENDIF
+
+            !=== Check for stop energy threshold ===!
+
+            IF ( estop ) THEN
+
+              IF ( e <= ef ) THEN
+                efpt(isim) = time
+                estop = .FALSE.
+              ENDIF
+
+            ENDIF
+
+          ENDDO
+
+        ENDDO
+
+        RETURN
+
+      END SUBROUTINE KFOLD
