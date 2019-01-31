6deec3e8cd23a5a216cbbe1d63934101d773e9ff
Alex Reis <areis1992@gmail.com>
2019-01-31 12:07:21 -0500

    debugging

--- src/python/tmp/find_k_conversion.py
+++ src/python/tmp/find_k_conversion.py
@@ -41,7 +41,7 @@ def custom_options(seq, initial_structure, maxtime):
         nsim=1,
         tmax=maxtime,
         trange=KFOLDWrapper.get_trange(maxtime),
-        pynsim=100
+        pynsim=50
         )
 
 calc_y = lambda a0,a1,x: a1*x+a0
