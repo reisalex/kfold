4ede9f39679cf702aa59a0c0fb473c51b9cdd739
Alex Reis <areis1992@gmail.com>
2019-01-31 12:24:11 -0500

    debugging

--- src/python/tmp/find_k_conversion.py
+++ src/python/tmp/find_k_conversion.py
@@ -76,8 +76,12 @@ def fun(x):
 
 def main():
     # minimize(fun, x0=[650.0], bounds=[(500.0,10000.0)])
-    for k in xrange(400.0,10000.0,50):
-        print k
+    x = [400.0]
+    rsqlist = []
+    while x[0] < 10000.0:
+        rsqlist.append(fun(x))
+        x[0] ++ 50.0
+    print rsqlist
 
 if __name__ == "__main__":
     main()
