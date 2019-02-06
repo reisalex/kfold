93d6af9d5d758f5293335363f1550526762fb72c
Alex Reis <areis1992@gmail.com>
2019-02-06 14:28:11 -0500

    Debuggin

--- src/python/tmp/find_k_conversion.py
+++ src/python/tmp/find_k_conversion.py
@@ -35,7 +35,8 @@ beta = 0.45
 
 def custom_options(seq, initial_structure, kvals, dG_final):
     fold=ViennaRNA.RNAfold(seq)
-    times = filter(None,[t if t < 10.0 else None for t in kvals*np.exp(-beta*dG_final)]).append(10.0)
+    times = filter(None,[t if t < 10.0 else None for t in kvals*np.exp(-beta*dG_final)])
+    times.append(10.0)
     print len(times)
     return dict(
         fold0=initial_structure,
