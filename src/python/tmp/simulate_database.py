765c2e94c0aed1edaa58419c15be49071f5b06ec
Alex Reis <areis1992@gmail.com>
2019-03-04 15:05:27 -0500

    Debugging

--- src/python/tmp/simulate_database.py
+++ src/python/tmp/simulate_database.py
@@ -29,7 +29,7 @@ def simulate():
     dG_16S_SDs = list(df['dG_SD_16S_hybrid'])
     cutoffs    = list(df['most_5p_paired_SD_mRNA'])
     seqs = [s[c:] for s,c in zip(used_seqs,cutoffs)]
-    options = [get_options(seq, k*np.exp(beta * dG_16S_SDs) ) for seq,dG_16S_SD in zip(seqs,dG_16S_SDs)]
+    options = [get_options(seq, k*np.exp(beta * dG_16S_SD) ) for seq,dG_16S_SD in zip(seqs,dG_16S_SDs)]
 
     mean_dGs = []; std_dGs = [];
     for output in KFOLDWrapper.run(seqs,options):
